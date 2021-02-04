c1de349950ab9dd5753501d4c5ff783e1fe4cb1a
Elijah Ozhmegov <ElijahOzhmegov@gmail.com>
2021-02-04 17:23:09 +0100

    add initial analysis

---
+++ initial_analysis/S&P500.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import mplfinance as fplt
+
+df = pd.read_csv("data/^GSPC.csv", index_col=0, parse_dates=True)
+
+fplt.plot(df,
+          type='candle',
+          title='S&P 500',
+          ylabel='Price ($)',
+          style='yahoo')
+
+fplt.plot(df,
+          type='candle', style='yahoo',
+          title='S&P500',
+          ylabel='Price ($)',
+          savefig='images/SP500_all.png')
+
+dt_range = pd.date_range(start="1927-12-30", end="1962-01-03")
+fplt.plot(df[df.index.isin(dt_range)],
+          type='candle', style='yahoo',
+          title='S&P500',
+          ylabel='Price ($)',
+          savefig='images/SP500_before_1962.png')
+
+fplt.plot(df[df.index.isin(dt_range)],
+          type='line', style='yahoo',
+          title='S&P500',
+          ylabel='Price ($)',
+          savefig='images/SP500_before_1962_line.png')
+
+# As you can see it does not make much sense to look at the candle
+# sticks before 1962-Jan-02. Before that date data is no full.
+
+dt_range = pd.date_range(start="1962-01-02", end="2021-01-22")
+df = df[df.index.isin(dt_range)]
+
+fplt.plot(df,
+          type='candle',
+          title='S&P 500',
+          ylabel='Price ($)',
+          style='yahoo',
+          savefig = 'images/SP500_after_1962.png')
